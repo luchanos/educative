0dd65207f451ddb74040ea68d20405c9c13442b8
luchanos <n.luchanos@gmail.com>
2025-05-21 01:06:37 +0200

    transcriber

--- automation/transcriber/main.py
+++ automation/transcriber/main.py
@@ -3,6 +3,7 @@ from moviepy.editor import VideoFileClip
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 import os
+from tqdm import tqdm
 
 
 def extract_audio_from_video(video_path: str, audio_path: str) -> None:
@@ -10,27 +11,31 @@ def extract_audio_from_video(video_path: str, audio_path: str) -> None:
     video.audio.write_audiofile(audio_path, codec='pcm_s16le')
 
 
-def recognize_speech_from_audio(audio_path: str, chunk_length_ms: int = 7000) -> str:
+def recognize_speech_from_audio(audio_path: str, chunk_length_ms: int = 60_000) -> str:
     audio = AudioSegment.from_wav(audio_path)
     chunks = make_chunks(audio, chunk_length_ms)
 
     recognizer = sr.Recognizer()
     transcription = []
 
-    for i, chunk in enumerate(chunks):
-        chunk_name = f"chunk{i}.wav"
-        chunk.export(chunk_name, format="wav")
-        with sr.AudioFile(chunk_name) as source:
-            audio_listened = recognizer.record(source)
-            try:
-                text = recognizer.recognize_google(audio_listened, language="ru-RU")
-                transcription.append(text)
-            except sr.UnknownValueError:
-                continue
-            except sr.RequestError as e:
-                print(f"Could not request results; {e}")
-                continue
-        os.remove(chunk_name)  # удаляем временный файл
+    with tqdm(total=len(chunks), desc="Transcribing", unit="chunk", leave=False) as pbar:
+        for i, chunk in enumerate(chunks):
+            chunk_name = f"chunk{i}.wav"
+            chunk.export(chunk_name, format="wav")
+
+            with sr.AudioFile(chunk_name) as source:
+                audio_listened = recognizer.record(source)
+                try:
+                    text = recognizer.recognize_google(audio_listened, language="ru-RU")
+                    transcription.append(text)
+                except sr.UnknownValueError:
+                    pass
+                except sr.RequestError as e:
+                    tqdm.write(f"[Error] Could not request results: {e}")
+
+            os.remove(chunk_name)
+            pbar.update(1)
+            pbar.set_postfix_str(f"{i + 1}/{len(chunks)} chunks")
 
     return " ".join(transcription)
 
